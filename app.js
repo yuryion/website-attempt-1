043fc41b4ffabf5be3b9a3ac9899ff2a0e8f8c25
yuryion <47436704+yuryion@users.noreply.github.com>
2022-06-13 14:23:59 -0500

    Added transitions to the sections of the website

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const sections = document.querySelectorAll('.section');
-const sctBtns = document.querySelectorAll('.controls'); //buttons parent
+const sectBtns = document.querySelectorAll('.controls'); //buttons parent
 const sectBtn = document.querySelectorAll('.control'); //the actual buttons
 const allSections = document.querySelector('.main-content'); //selects the body
 
@@ -8,10 +8,30 @@ function PageTransitions(){
     for(let i = 0; i < sectBtn.length; i++){
         sectBtn[i].addEventListener('click', function(){ //add event listener to every button
             let currentBtn = document.querySelectorAll('.active-btn');  //get the current button
-            currentBtn[0].className = currentBtn[0].className.replace('active-btn','');  //remove it as the active button
+            currentBtn[0].className = currentBtn[0].className.replace(' active-btn','');  //remove it as the active button
             this.className += ' active-btn'; //activate the button that was clicked
         })
     }
+
+    //Sections active class
+    allSections.addEventListener('click', (e) => {
+        const id = e.target.dataset.id;
+        if (id){ //if id exists
+            //remove active from the other button
+            sectBtns.forEach((btn) => {
+                btn.classList.remove('active');
+            })
+            e.target.classList.add('active');
+            
+            //hide other sections
+            sections.forEach((section) => {
+                section.classList.remove('active');
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
 }
 
 PageTransitions();
